cc787cc9a50e61b9c3d692f85485537f854148ae
Pierre Migeon <pmigeon@e1z3r6p23.42.us.org>
2018-09-24 12:24:10 -0700

    fixed strstr & strnstr bug

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -1,37 +1,49 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pmigeon <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/09/24 10:47:18 by pmigeon           #+#    #+#             */
-/*   Updated: 2018/09/24 12:20:26 by pmigeon          ###   ########.fr       */
+/*   Created: 2018/09/24 11:51:31 by pmigeon           #+#    #+#             */
+/*   Updated: 2018/09/24 12:16:33 by pmigeon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
 
-char	*ft_strstr(const char *haystack, const char *needle)
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	int i;
+	size_t j;
 
 	i = 0;
+	j = 0;
 	if (!*needle)
-		return ((char *)haystack); 
-	while (*haystack)
+		return ((char *)haystack);
+	while (j < len)
 	{
-		if (*haystack == needle[i])
+		if (haystack[j] == needle[i])
 			++i;
 		else
 		{
 			i = 0;
-			if (*haystack == needle[i])
-				haystack--;
+			if (haystack[j] == needle[i])
+				--j;
 		}
 		if (needle[i] == '\0')
-			return ((char *)haystack - --i);
-		haystack++;
+			return ((char *)&haystack[j - --i]);
+		j++;
 	}
 	return (NULL);
+}
+
+#include <stdio.h>
+int		main()
+{
+	char str[] = "abchello";
+	char str2[] = "hello";
+	printf("%p\n", ft_strnstr(str, str2, 8));
+	printf("%p\n", strnstr(str, str2, 8));
+	return (0);
 }
